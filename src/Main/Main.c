149f3e316bd327576edfc459df889c17a704f5c1
skferrei <samikoe.ferreira@gmail.com>
2025-03-19 10:33:27 +0100

    first modif of main.c

--- src/Main/Main.c
+++ src/Main/Main.c
@@ -1,5 +1,16 @@
 //Inculde the header file
 
+//id unsigned long
+typedef struct joueur{
+    Role role,
+    id pion,
+    char numero,
+    int argent,
+    cartes * carte_jouee,
+    id lieu
+}Joueur;
+
+Joueur joueur[8];
 
 
 int main(int argc, char *argv[])
@@ -7,7 +18,7 @@ int main(int argc, char *argv[])
 
     while (1)
     {
-
+        
     }
 
     return 0;
