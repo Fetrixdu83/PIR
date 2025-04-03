fec44bc3d3ac190f9d07d6fa0d640fccbdae6e83
Juxin233 <chenjuxin233@gmail.com>
2025-04-03 11:14:27 +0200

    Initialisation de card BH

--- src/Roles/src/BH.c
+++ src/Roles/src/BH.c
@@ -21,35 +21,110 @@ Player* BH_create_player() //Create a player with the BH role
 }
 
 
-void BH_play(Player* player, id card, char target) //Play function for BH
+int BH_play(Player* player, id card, char target) //Play function for BH
 {
     switch(card){
-        case 0x0001000: // BH card 0 AKA Common card
+        case CARD_COMMON: // BH card 0 AKA Common card
             // Implement the effect of BH Common card
+            switch (player->place):
+                case PLACE_BANK:
+                    player->money += 3; // Gain 3 money
+                    return SUCCESS; // Card played successfully
+                    break;
+                case PLACE_CYPERCAFE:
+                    return FAILURE_WRONG_PLACE; // Card played in a wrong place
+                    break;
+                case PLACE_LIBRARY:
+                    if(player->money >= 2) // Check if player has enough money
+                        player->money -= 2; // Lose 2 money
+                        return SUCCESS; // Card played successfully
+                    else
+                        return FAILURE_NOT_ENOUGH_MONEY; 
+                    break;
+                case PLACE_ENTREPRISE:
+                    return FAILURE_WRONG_PLACE; 
+                    break;
             break;
-        case 0x0001001: // BH card 1
+        case BH_CORRUPT: // BH card 1
             // Implement the effect of BH card 1
+            if(player->money >= 2){
+                player->money -= 2; 
+                
+                return SUCCESS; // Card played successfully
+            }else{
+                return FAILURE_NOT_ENOUGH_MONEY; // Not enough money to play the card
+            
+            }    
             break; 
-        case 0x0001002: // BH card 2
+        case BH_BOTNET: // BH card 2
             // Implement the effect of BH card 2
+            if(player->money >= 1){
+                player->money -= 1; 
+                return SUCCESS; // Card played successfully
+            }else{
+                return FAILURE_NOT_ENOUGH_MONEY; // Not enough money to play the card
+            
+            } 
             break;
-        case 0x0001003: // BH card 3
+        case BH_DDoS: // BH card 3
             // Implement the effect of BH card 3
+            if(player->money >= 1){
+                player->money -= 1; 
+                return SUCCESS; // Card played successfully
+            }else{
+                return FAILURE_NOT_ENOUGH_MONEY; // Not enough money to play the card
+            
+            } 
             break;
-        case 0x0001004: // BH card 4    
+        case BH_BRUTE_FORCE: // BH card 4    
             // Implement the effect of BH card 4
+            if(player->money >= 1){
+                player->money -= 1; 
+                return SUCCESS; // Card played successfully
+            }else{
+                return FAILURE_NOT_ENOUGH_MONEY; // Not enough money to play the card
+            
+            } 
             break;
-        case 0x0001005: // BH card 5
+        case BH_PHISHING: // BH card 5
             // Implement the effect of BH card 5
+            if(player->money >= 2){
+                player->money -= 2; 
+                return SUCCESS; // Card played successfully
+            }else{
+                return FAILURE_NOT_ENOUGH_MONEY; // Not enough money to play the card
+            
+            } 
             break;
-        case 0x0001006: // BH card 6    
+        case BH_SQL_INJECTION: // BH card 6    
             // Implement the effect of BH card 6
+            if(player->money >= 3){
+                player->money -= 3; 
+                return SUCCESS; // Card played successfully
+            }else{
+                return FAILURE_NOT_ENOUGH_MONEY; // Not enough money to play the card
+            
+            } 
             break;
-        case 0x0001007: // BH card 7    
+        case BH_XSS: // BH card 7    
             // Implement the effect of BH card 7
+            if(player->money >= 4){
+                player->money -= 4; 
+                return SUCCESS; // Card played successfully
+            }else{
+                return FAILURE_NOT_ENOUGH_MONEY; // Not enough money to play the card
+            
+            } 
             break;
-        case 0x0001008: // BH card 8
+        case BH_PHYSICAL_ATTACK: // BH card 8
             // Implement the effect of BH card 8
+            if(player->money >= 3){
+                player->money -= 3; 
+                return SUCCESS; // Card played successfully
+            }else{
+                return FAILURE_NOT_ENOUGH_MONEY; // Not enough money to play the card
+            
+            } 
             break;
     }
 }
